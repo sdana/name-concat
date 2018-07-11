0bf8f08aabcf0ea5bbd315166050086b3900d16e
sdana <seth.dana@gmail.com>
2018-07-11 09:57:43 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// 1. Write a function that takes two arguments. This function should return the concatenated value of the two arguments.
+
+// 2. Call the function with your first name and last name as the two arguments.
+
+// 3. Console log the return value.
+
+const concatValue = (value1, value2) => {
+    return `${value1} ${value2}`
+}
+
+myName = concatName("Seth", "Dana")
+console.log(myName)
